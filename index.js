d7e823a755b752ff175625b661292323715ed945
Ajay <kereajay578@gmail.com>
2024-04-14 22:41:17 +0530

    adding all the files of git api

---
+++ index.js
@@ -0,0 +1,79 @@
+let contentdisplay = document.getElementById("display");
+let searchbtn = document.getElementById("searchbtn");
+let input1 = document.getElementById("search");
+// searchbtn.addEventListener('click',()=>{
+
+async function gitdata() {
+    try{
+  let res = await fetch(`https://api.github.com/users/${input1.value}`);
+  let data = await res.json();
+  console.log(data);
+
+  contentdisplay.innerHTML = ` <div id="jsmain" style="width: 80%; margin:auto;">
+    <div id="a" style="display: flex; flex-direction: row; justify-content: space-between;">
+        <div id="profile" style="display: flex;flex-direction: row; ">
+
+            <img src="${data.avatar_url}" alt="" height="80px" width="80px"
+                style="border-radius: 50%;">
+            <ul type="none">
+                <li>
+                    <h2>${data.name}</h2>
+                </li>
+
+                <li><a href="${data.html_url}">
+                        <p>@${data.login}</p>
+                    </a></li>
+            </ul>
+        </div>
+        <div id="joindate">
+            <p id="dates">${data.created_at.slice(0, 10)}</p>
+        </div>
+        
+    </div>
+    <h4>
+        <p>${data.bio}</p>
+    </h4>
+    <div id="b" style="background-color: white; display: flex; flex-direction: row; justify-content: space-between; padding: 10px;">
+        <ul type="none">
+            <li>Repos</li>
+            <li>${data.public_repos}</li>
+        </ul>
+        <ul type="none">
+            <li>Followers</li>
+            <li>${data.followers}</li>
+        </ul>
+        <ul type="none">
+            <li>Following</li>
+            <li>${data.following}</li>
+        </ul>
+
+    </div>
+    <div id="c" style="display: flex;flex-direction: row;justify-content: space-between;">
+        <ul type="none">
+            <li style="font-size: large;"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pin-map-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z"/>
+                <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"/>
+              </svg>  <span>${data.location}</span></li>
+            <li style="font-size: large;"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+              </svg> <span>${data.blog}</span></li>
+        </ul>
+        <ul type="none">
+            <li style="font-size: large;"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
+              </svg> <span>${data.twitter_username}</span></li>
+              <li style="font-size: large;"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-building-fill" viewBox="0 0 16 16">
+                <path d="M3 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h3v-3.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V16h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm1 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5M4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5"/>
+              </svg> <span>${data.company}</span></li>
+        </ul>
+    </div>
+</div>    `;
+    
+}
+catch(error){
+    contentdisplay.innerHTML="Enter valid username"
+}
+}
+
+searchbtn.addEventListener('click',gitdata);
